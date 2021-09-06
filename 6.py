4d2a78f5b5c4a0ba241b001cac15b9afa835fd81
DeathSkor <76102974+DeathSkor@users.noreply.github.com>
2021-09-06 11:55:16 +0200

    Update 6.py

--- 6.py
+++ 6.py
@@ -2,10 +2,6 @@ from math import *
 def racine(x):
     if x>=0:
         a=sqrt(x)
-        b=-a
-        if a==0:
-            return 0
-        else: return a,b
     elif x<0:
         return "impossible"
     
